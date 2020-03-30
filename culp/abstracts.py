f29d907ce7f11ae45db25581169a44788b23344f
amin <amenfd@gmail.com>
2020-03-30 23:47:30 +0430

    refactoor link_predictors module

--- culp/abstracts.py
+++ culp/abstracts.py
@@ -1,7 +1,24 @@
 from abc import abstractmethod, ABC
 
+from culp.leg import Leg
+
 
 class SimilarityEdgesStrategy(ABC):
     @abstractmethod
     def find_neighbors(self):
+        pass
+
+
+class ClassScoresStrategy(ABC):
+    leg: Leg
+    class_indices: range
+    test_indices: range
+
+    def load_leg(self, leg: Leg):
+        self.leg = leg
+        self.class_indices = range(self.leg.n + self.leg.m, self.leg.n + self.leg.m + self.leg.c)
+        self.test_indices = range(self.leg.n, self.leg.n + self.leg.m)
+
+    @abstractmethod
+    def compute_class_scores(self):
         pass
--- culp/link_predictors.py
+++ culp/link_predictors.py
@@ -1,60 +1,44 @@
 import numpy
 
-
-def common_neighbors(leg, test_indices: tuple, class_indices: tuple):
-    assert (isinstance(test_indices, tuple) and len(test_indices) == 2)
-    assert (isinstance(class_indices, tuple) and len(class_indices) == 2)
-    N = len(range(*test_indices))
-    C = len(range(*class_indices))
-    similarities = numpy.zeros((N, C), dtype=float)
-    for class_index, j in enumerate(range(*class_indices)):
-        for test_index, i in enumerate(range(*test_indices)):
-            similarities[test_index, class_index] = leg.count_common_neighbors(i, j)
-
-    prediction = numpy.argmax(similarities, axis=1)
-    return prediction
+from culp.abstracts import ClassScoresStrategy
 
 
-def adamic_adar(leg, test_indices: tuple, class_indices: tuple):
-    assert (isinstance(test_indices, tuple) and len(test_indices) == 2)
-    assert (isinstance(class_indices, tuple) and len(class_indices) == 2)
-    N = len(range(*test_indices))
-    C = len(range(*class_indices))
-    similarities = numpy.zeros((N, C), dtype=float)
-    for class_index, j in enumerate(range(*class_indices)):
-        for test_index, i in enumerate(range(*test_indices)):
-            for n in leg.common_neighbors(i, j):
-                similarities[test_index, class_index] += 1 / (numpy.log(leg.degree(n)) + 10e-10)
-    prediction = numpy.argmax(similarities, axis=1)
-    return prediction
+class CommonNeighborsStrategy(ClassScoresStrategy):
+    def compute_class_scores(self):
+        similarities = numpy.zeros((self.leg.n, self.leg.c), dtype=float)
+        for class_index, j in enumerate(self.class_indices):
+            for test_index, i in enumerate(self.test_indices):
+                similarities[test_index, class_index] = self.leg.count_common_neighbors(i, j)
+        return similarities
 
 
-def resource_allocation_index(leg, test_indices: tuple, class_indices: tuple):
-    assert (isinstance(test_indices, tuple) and len(test_indices) == 2)
-    assert (isinstance(class_indices, tuple) and len(class_indices) == 2)
-    N = len(range(*test_indices))
-    C = len(range(*class_indices))
-    similarities = numpy.zeros((N, C), dtype=float)
-    for class_index, j in enumerate(range(*class_indices)):
-        for test_index, i in enumerate(range(*test_indices)):
-            for n in leg.common_neighbors(i, j):
-                similarities[test_index, class_index] += 1 / leg.degree(n)
-
-    prediction = numpy.argmax(similarities, axis=1)
-    return prediction
+class AdamicAdarStrategyStrategy(ClassScoresStrategy):
+    def compute_class_scores(self):
+        similarities = numpy.zeros((self.leg.n, self.leg.c), dtype=float)
+        for class_index, j in enumerate(self.class_indices):
+            for test_index, i in enumerate(self.test_indices):
+                for n in self.leg.common_neighbors(i, j):
+                    similarities[test_index, class_index] += 1 / (numpy.log(self.leg.degree(n)) + 10e-10)
+        return similarities
 
 
-def compatibility_score(leg, test_indices: tuple, class_indices: tuple):
-    assert (isinstance(test_indices, tuple) and len(test_indices) == 2)
-    assert (isinstance(class_indices, tuple) and len(class_indices) == 2)
-    N = len(range(*test_indices))
-    C = len(range(*class_indices))
-    similarities = numpy.zeros((N, C), dtype=float)
-    for class_index, j in enumerate(range(*class_indices)):
-        for test_index, i in enumerate(range(*test_indices)):
-            for n in leg.common_neighbors(i, j):
-                deg1 = leg.degree(n) - leg.count_common_neighbors(n, j)
-                deg2 = leg.degree(n) - leg.count_common_neighbors(n, i)
-                similarities[test_index, class_index] += (1 / deg1 + 1 / deg2)
-    prediction = numpy.argmax(similarities, axis=1)
-    return prediction
+class ResourceAllocationIndexStrategy(ClassScoresStrategy):
+    def compute_class_scores(self):
+        similarities = numpy.zeros((self.leg.n, self.leg.c), dtype=float)
+        for class_index, j in enumerate(self.class_indices):
+            for test_index, i in enumerate(self.test_indices):
+                for n in self.leg.common_neighbors(i, j):
+                    similarities[test_index, class_index] += 1 / self.leg.degree(n)
+        return similarities
+
+
+class CompatibilityScoreStrategy(ClassScoresStrategy):
+    def compute_class_scores(self):
+        similarities = numpy.zeros((self.leg.n, self.leg.c), dtype=float)
+        for class_index, j in enumerate(self.class_indices):
+            for test_index, i in enumerate(self.test_indices):
+                for n in self.leg.common_neighbors(i, j):
+                    deg1 = self.leg.degree(n) - self.leg.count_common_neighbors(n, j)
+                    deg2 = self.leg.degree(n) - self.leg.count_common_neighbors(n, i)
+                    similarities[test_index, class_index] += (1 / deg1 + 1 / deg2)
+        return similarities
