9b65b1198fb54ccd45c0b168acc5f65dab72831c
amin <amenfd@gmail.com>
2020-04-09 22:52:38 +0430

    add concurrency to link predictors

--- culp/abstracts.py
+++ culp/abstracts.py
@@ -1,4 +1,8 @@
 from abc import abstractmethod, ABC
+from multiprocessing import Pool
+from typing import Union
+
+import numpy
 
 from .leg import Leg
 
@@ -13,12 +17,27 @@ class ClassScoresStrategy(ABC):
     leg: Leg
     class_indices: range
     test_indices: range
+    n_jobs: int
 
-    def load_leg(self, leg: Leg):
+    def load_leg(self, leg: Leg, n_jobs=1):
         self.leg = leg
+        self.n_jobs = n_jobs
         self.class_indices = range(self.leg.n + self.leg.m, self.leg.n + self.leg.m + self.leg.c)
         self.test_indices = range(self.leg.n, self.leg.n + self.leg.m)
 
-    @abstractmethod
     def compute_class_scores(self):
+        parameters = [
+            (i, j)
+            for j in self.test_indices
+            for i in self.class_indices
+        ]
+        pool = Pool(self.n_jobs)
+        results = pool.starmap(self.find_score, parameters)
+        pool.close()
+        pool.join()
+        similarities = numpy.reshape(numpy.array(results), (self.leg.m, self.leg.c))
+        return similarities
+
+    @abstractmethod
+    def find_score(self, class_node: int, test_node: int) -> Union[int, float]:
         pass
--- culp/link_predictors.py
+++ culp/link_predictors.py
@@ -4,41 +4,39 @@ from .abstracts import ClassScoresStrategy
 
 
 class CommonNeighborsStrategy(ClassScoresStrategy):
-    def compute_class_scores(self):
-        similarities = numpy.zeros((self.leg.m, self.leg.c), dtype=float)
-        for class_index, j in enumerate(self.class_indices):
-            for test_index, i in enumerate(self.test_indices):
-                similarities[test_index, class_index] = self.leg.count_common_neighbors(i, j)
-        return similarities
+    __name__ = 'CN'
+
+    def find_score(self, class_node, test_node):
+        return self.leg.count_common_neighbors(class_node, test_node)
 
 
-class AdamicAdarStrategyStrategy(ClassScoresStrategy):
-    def compute_class_scores(self):
-        similarities = numpy.zeros((self.leg.m, self.leg.c), dtype=float)
-        for class_index, j in enumerate(self.class_indices):
-            for test_index, i in enumerate(self.test_indices):
-                for n in self.leg.common_neighbors(i, j):
-                    similarities[test_index, class_index] += 1 / (numpy.log(self.leg.degree(n)) + 10e-10)
-        return similarities
+class AdamicAdarStrategy(ClassScoresStrategy):
+    __name__ = 'AA'
+
+    def find_score(self, class_node, test_node):
+        score = 0
+        for n in self.leg.common_neighbors(class_node, test_node):
+            score += 1 / (numpy.log(self.leg.degree(n)) + 10e-10)
+        return score
 
 
-class ResourceAllocationIndexStrategy(ClassScoresStrategy):
-    def compute_class_scores(self):
-        similarities = numpy.zeros((self.leg.m, self.leg.c), dtype=float)
-        for class_index, j in enumerate(self.class_indices):
-            for test_index, i in enumerate(self.test_indices):
-                for n in self.leg.common_neighbors(i, j):
-                    similarities[test_index, class_index] += 1 / self.leg.degree(n)
-        return similarities
+class ResourceAllocationStrategy(ClassScoresStrategy):
+    __name__ = 'RA'
+
+    def find_score(self, class_node, test_node):
+        score = 0
+        for n in self.leg.common_neighbors(class_node, test_node):
+            score += 1 / self.leg.degree(n)
+        return score
 
 
 class CompatibilityScoreStrategy(ClassScoresStrategy):
-    def compute_class_scores(self):
-        similarities = numpy.zeros((self.leg.m, self.leg.c), dtype=float)
-        for class_index, j in enumerate(self.class_indices):
-            for test_index, i in enumerate(self.test_indices):
-                for n in self.leg.common_neighbors(i, j):
-                    deg1 = self.leg.degree(n) - self.leg.count_common_neighbors(n, j)
-                    deg2 = self.leg.degree(n) - self.leg.count_common_neighbors(n, i)
-                    similarities[test_index, class_index] += (1 / deg1 + 1 / deg2)
-        return similarities
+    __name__ = 'CS'
+
+    def find_score(self, class_node, test_node):
+        score = 0
+        for neighbor_node in self.leg.common_neighbors(class_node, test_node):
+            deg1 = self.leg.degree(neighbor_node) - self.leg.count_common_neighbors(neighbor_node, test_node)
+            deg2 = self.leg.degree(neighbor_node) - self.leg.count_common_neighbors(neighbor_node, class_node)
+            score += (1 / deg1 + 1 / deg2)
+        return score
